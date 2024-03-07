753fca3355d046e2f2c33722e4b3f9c55efac006
haseebulamin <haseebulamin07@gmail.com>
2024-03-07 23:26:44 +0500

    import-export

---
+++ src/Components/JavaScript-refresh/import-export/Export.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Export() {
+  return (
+    <div>
+        i'm Export file.
+        i'm imported in import file.
+        </div>
+  )
+}
---
+++ src/Components/JavaScript-refresh/import-export/Import.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Export from './Export'
+export default function Import() {
+  return (
+   <div>
+    i'm import file. Export file exported in me.
+    <Export/>
+   </div>
+  )
+}
