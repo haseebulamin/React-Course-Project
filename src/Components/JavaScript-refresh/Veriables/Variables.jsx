2daa816ed97b3244fe5437a3b0853632f4f813b2
haseebulamin <haseebulamin07@gmail.com>
2024-03-07 23:43:42 +0500

    variables

--- src/Components/JavaScript-refresh/Veriables/Variables.jsx
+++ src/Components/JavaScript-refresh/Veriables/Variables.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default function Variables() {
     let st = "haseeb ul amin";
+    let num = 33;
   return (
-    <div>Variables
-     {st}
+    <div>
+        Variables <br />
+        String:  {st} <br />
+        Number:   {num} <br />
+        Boolean:  true <br />
+        null <br /> undefined
     </div>
   )
 }
