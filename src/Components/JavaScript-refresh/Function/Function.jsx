bebcfd3fd6e4c58294925307c1cc40bb77574290
haseebulamin <haseebulamin07@gmail.com>
2024-03-08 23:55:50 +0500

    arrow func

--- src/Components/JavaScript-refresh/Function/Function.jsx
+++ src/Components/JavaScript-refresh/Function/Function.jsx
@@ -8,21 +8,35 @@ export default function Function() {
     
     function para(user,msg="what's up?")  // function with parameter
     {
-        console.log(user);
-        console.log(msg);
+        console.log(user,msg);
     }
+
     function returning(user,msg) // returning function
     {
         return "Hi, i'm " + user + ". " + msg;
     }
+    
+    const arrowFunc = (msg)=> // arrow function
+    {
+        console.log(msg)
+    }
+    
     greet();
     para("Alex","how are you!");
     para("Tom");
     const rtrn = returning("Hasii","just focus on work.");
     console.log(rtrn);
+    arrowFunc("I'm arrow function.");
+
   return (
     <div>
         Function
+        <ol>
+            <li>Simple function</li>
+            <li>Function With parameter</li>
+            <li>Returning function</li>
+            <li>Arrow function</li>
+        </ol>
     </div>
   )
 }
