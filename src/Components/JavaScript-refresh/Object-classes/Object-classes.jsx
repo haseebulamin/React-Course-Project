28f7943ec2ad31495c2e6625e7b02c3b2650b98f
haseebulamin <haseebulamin07@gmail.com>
2024-03-09 00:15:25 +0500

    object with func

--- src/Components/JavaScript-refresh/Object-classes/Object-classes.jsx
+++ src/Components/JavaScript-refresh/Object-classes/Object-classes.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 export default function ObjectClasses() {
     const student = {
         name: "Haseeb Ul Amin",
-        age: 18
+        age: 18,
+        greet(msg){
+            console.log(msg);
+        }
     }
     console.log(student);
     console.log(student.name);
     console.log(student.age);
+    student.greet("Hello React!");
   return (
     <div>
         Object-classes
