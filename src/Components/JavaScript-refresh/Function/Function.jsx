b7f66b6231a9ba0b69ed52f2461e788767063e36
haseebulamin <haseebulamin07@gmail.com>
2024-03-08 23:38:30 +0500

    func with parameter

--- src/Components/JavaScript-refresh/Function/Function.jsx
+++ src/Components/JavaScript-refresh/Function/Function.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 export default function Function() {
-    function greet() {
+    function greet()  // simple function
+    {
         console.log("Hello, Haseeb Ul Amin");
     }
+    
+    function para(user,msg="what's up?")  // function with parameter
+    {
+        console.log(user);
+        console.log(msg);
+    }
+    
     greet();
+    para("Alex","how are you!");
+    para("Tom");
   return (
-    <div>Function</div>
+    <div>
+        Function
+    </div>
   )
 }
