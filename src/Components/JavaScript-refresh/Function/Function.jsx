1dd0625847d95e6b695de8e8c19064ca3e6a4c25
haseebulamin <haseebulamin07@gmail.com>
2024-03-08 23:21:59 +0500

    simple func

--- src/Components/JavaScript-refresh/Function/Function.jsx
+++ src/Components/JavaScript-refresh/Function/Function.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function Function() {
+    function greet() {
+        console.log("Hello, Haseeb Ul Amin");
+    }
+    greet();
   return (
     <div>Function</div>
   )
