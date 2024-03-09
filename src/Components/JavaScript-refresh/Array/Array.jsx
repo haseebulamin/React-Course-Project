315850aa0bb6cd3a93842066d718211784439a6f
haseebulamin <haseebulamin07@gmail.com>
2024-03-10 01:30:22 +0500

    simple array

--- src/Components/JavaScript-refresh/Array/Array.jsx
+++ src/Components/JavaScript-refresh/Array/Array.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function Array() {
+    let hobbies = ["Football","Cricket","Hockey","Vollyball"]; // simple array
+    console.log(hobbies);
+    console.log(hobbies[0]);
+    console.log(hobbies[1]);
+    console.log(hobbies[2]);
+    console.log(hobbies[3]);
   return (
     <div>Array</div>
   )
