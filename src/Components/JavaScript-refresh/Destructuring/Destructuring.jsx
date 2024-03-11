e83df8a12a7a2326827ccebb91120f8cee3d6061
haseebulamin <haseebulamin07@gmail.com>
2024-03-11 22:38:00 +0500

    part with array

--- src/Components/JavaScript-refresh/Destructuring/Destructuring.jsx
+++ src/Components/JavaScript-refresh/Destructuring/Destructuring.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function Destructuring() {
+
+    let username = ["Tom", "Alis"];
+    let firtsname = username[0];
+    let lastname = username[1];
+    console.log(firtsname,lastname);
+    
   return (
     <div>Destructuring</div>
   )
