6ba4bec7e176de6799264f8d6f40281eda4c3a4e
haseebulamin <haseebulamin07@gmail.com>
2024-03-11 22:48:23 +0500

    part with destructure

--- src/Components/JavaScript-refresh/Destructuring/Destructuring.jsx
+++ src/Components/JavaScript-refresh/Destructuring/Destructuring.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 export default function Destructuring() {
-
+    // partation with array 
     let username = ["Tom", "Alis"];
     let firtsname = username[0];
     let lastname = username[1];
     console.log(firtsname,lastname);
-    
+
+    // this code in preerable instead of above 
+    const [onePart,twoPart] = ["Haseeb","Ul Amin"];
+    console.log(onePart,twoPart);
+ 
+    // partation with object 
+    let user = {
+        name: "Ibrar",
+        Age: 20
+    }
+    console.log(user.name)
+    console.log(user.Age)
   return (
     <div>Destructuring</div>
   )
