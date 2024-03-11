224e48e6b1ec2d78758bbe57eccf9202adc8d95e
haseebulamin <haseebulamin07@gmail.com>
2024-03-11 23:06:42 +0500

    if else

--- src/Components/JavaScript-refresh/Control-structure/ControlStructure.jsx
+++ src/Components/JavaScript-refresh/Control-structure/ControlStructure.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default function ControlStructure() {
+    const number = prompt("Enter Value: ");
+
+    if (number === "1") 
+    {
+        console.log("i am 1.")
+    } 
+    else  if (number === "2") 
+    {
+        console.log("i am 2.")
+    } 
+    else  if (number === "3")  {
+        console.log("i am 3.")
+    } else {
+            console.log("invalid number.")
+       
+    }
   return (
     <div>ControlStructure</div>
   )
