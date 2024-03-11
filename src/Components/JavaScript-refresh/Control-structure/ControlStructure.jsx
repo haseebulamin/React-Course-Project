e573d03acc7e44c48c63034a5d84e1d5068c1225
haseebulamin <haseebulamin07@gmail.com>
2024-03-11 23:11:22 +0500

    for loop

--- src/Components/JavaScript-refresh/Control-structure/ControlStructure.jsx
+++ src/Components/JavaScript-refresh/Control-structure/ControlStructure.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function ControlStructure() {
     const number = prompt("Enter Value: ");
 
+    // if else --conditions
     if (number === "1") 
     {
         console.log("i am 1.")
@@ -14,10 +15,22 @@ export default function ControlStructure() {
     else  if (number === "3")  {
         console.log("i am 3.")
     } else {
-            console.log("invalid number.")
-       
+            console.log("invalid number.")  
+    }
+ 
+    // for loop 
+    let games = ["football", "Vollyball", "hockey", "soccor", "cricket"];
+
+    for (let game of games) 
+    {
+        console.log(game);
     }
   return (
-    <div>ControlStructure</div>
+    <div>
+        Control Structure <br />
+        -- if else <br />
+        -- for loop 
+
+    </div>
   )
 }
