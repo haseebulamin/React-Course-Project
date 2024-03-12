d20d43e0a320cf527434280e97badad7a1fa8eb3
haseebulamin <haseebulamin07@gmail.com>
2024-03-12 21:43:10 +0500

    simple marge array

--- src/Components/JavaScript-refresh/Spread-Operator/SpreadOperator.jsx
+++ src/Components/JavaScript-refresh/Spread-Operator/SpreadOperator.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export default function SpreadOperator() {
+
+    let hobbies = ["Football","Cricket","Hockey","Vollyball"];
+    let newhobbies = ["Reading"];
+    let margeArray = [hobbies,newhobbies]; // simple marge
+    console.log(margeArray);
+
   return (
     <div>
         Spread Operator
