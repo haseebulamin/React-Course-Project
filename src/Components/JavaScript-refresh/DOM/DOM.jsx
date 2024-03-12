3f6eb049518cf98378a8e8eb002ace7c16b892db
haseebulamin <haseebulamin07@gmail.com>
2024-03-13 01:21:21 +0500

    dom

--- src/Components/JavaScript-refresh/DOM/DOM.jsx
+++ src/Components/JavaScript-refresh/DOM/DOM.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 export default function DOM() {
+    document.getElementById("demo").innerHTML = "Hello World!";
   return (
     <div>
         DOM
-        <ul>
+        <ul className='list'>
             <li>hello</li>
             <li>hi</li>
+            <li id='demo'></li>
         </ul>
     </div>
   )
