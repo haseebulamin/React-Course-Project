e5770bf9e42e5fc1f66a6e49b63a44ec3ce2b9f5
haseebulamin <haseebulamin07@gmail.com>
2024-03-13 22:00:05 +0500

    Reference Vs Primitve

--- src/Components/JavaScript-refresh/Reference-VS-Primitve/ReferenceVsPrimitve.jsx
+++ src/Components/JavaScript-refresh/Reference-VS-Primitve/ReferenceVsPrimitve.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
 export default function ReferenceVsPrimitve() {
+
+    let user = "Haseeb";
+    console.log(user);
+    user = "Haseeb Ul Amin";
+    console.log(user);
+
+    user = user.concat(" Developer");
+    console.log(user);
+
+    const hobbies = ["Hockey", "Football"];
+    console.log(hobbies);
+    hobbies.push("Cricket");
+    console.log(hobbies);
   return (
     <div>
         Reference Vs Primitve
