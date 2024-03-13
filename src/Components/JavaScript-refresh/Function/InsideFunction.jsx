a6b245d82385f173519c3b3d4d92f26d9555ba12
haseebulamin <haseebulamin07@gmail.com>
2024-03-13 21:46:37 +0500

    inside func with paramtr

--- src/Components/JavaScript-refresh/Function/InsideFunction.jsx
+++ src/Components/JavaScript-refresh/Function/InsideFunction.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 export default function InsideFunction() {
 
-    
+    function init(value) // with parameter
+    {
+        function greet(value) {
+            console.log(value)
+        }
+        greet(value);
+    }
+    init("Hello! Function inside function");
   return (
     <div>
         Function inside function
