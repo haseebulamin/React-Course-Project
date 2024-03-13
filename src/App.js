8940d74b3efc1c562a8100ebb8cdb64bcc28c88a
haseebulamin <haseebulamin07@gmail.com>
2024-03-13 21:51:18 +0500

    refrence file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import InsideFunction from './Components/JavaScript-refresh/Function/InsideFunction';
+import ReferenceVsPrimitve from './Components/JavaScript-refresh/Reference-VS-Primitve/ReferenceVsPrimitve';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <InsideFunction/>
+       <ReferenceVsPrimitve/>
       </header>
     </div>
   );
---
+++ src/Components/JavaScript-refresh/Reference-VS-Primitve/ReferenceVsPrimitve.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ReferenceVsPrimitve() {
+  return (
+    <div>
+        Reference Vs Primitve
+        </div>
+  )
+}
