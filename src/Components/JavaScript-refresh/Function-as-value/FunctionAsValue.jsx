f8a050b04adeccf0ff7fae22026ee6506eebb487
haseebulamin <haseebulamin07@gmail.com>
2024-03-13 21:34:19 +0500

    set timeout func

--- src/Components/JavaScript-refresh/Function-as-value/FunctionAsValue.jsx
+++ src/Components/JavaScript-refresh/Function-as-value/FunctionAsValue.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 export default function FunctionAsValue() {
+    function handleTimeout1()
+    {
+     console.log("Time out!");   
+    }
+    const handleTimeout2 = ()=>{
+     console.log("Time out .... Again!");
+    }
+    setTimeout(handleTimeout1, 2000);
+    setTimeout(handleTimeout2, 3000);
+    setTimeout(()=>{
+     console.log("More Timing out!");   
+    }, 4000)
   return (
-    <div>FunctionAsValue</div>
+    <div>
+        Function As Value
+        </div>
   )
 }
