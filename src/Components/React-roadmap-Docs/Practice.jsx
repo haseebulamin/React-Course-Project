9a1ed4922ab5337f4260cf424e68d363a5f295b4
haseebulamin <haseebulamin07@gmail.com>
2024-05-10 18:49:52 +0500

    form

--- src/Components/React-roadmap-Docs/Practice.jsx
+++ src/Components/React-roadmap-Docs/Practice.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Practice() {
   return (
-    <div>Practice</div>
+    <div>
+      <input type="text" placeholder='Name'/><br />
+      <input type="text" placeholder='Email'/><br />
+      <input type="text" placeholder='Number'/><br />
+      <button type='submit'>Submit</button>
+
+    </div>
   )
 }
