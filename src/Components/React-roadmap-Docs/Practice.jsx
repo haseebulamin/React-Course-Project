88e27a989eab2b101002d6c0a4866f71b20cb626
haseebulamin <haseebulamin07@gmail.com>
2024-05-11 23:51:49 +0500

    flow

--- src/Components/React-roadmap-Docs/Practice.jsx
+++ src/Components/React-roadmap-Docs/Practice.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Practice() {
+  const [tech, setTech] = useState("");
+
+  const flow = ()=>{
+    
+      setTech("3")
+    
+    setInterval(() => {
+      setTech("2")
+    }, 1000);
+    setInterval(() => {
+      setTech("1")
+    }, 2000);
+
+  }
+
   return (
     <div>
 
-      <span>Name:</span><input type="text" placeholder='Name'/><br />
+       <h1>{tech}</h1>
+       <button onClick={flow}>Submit</button>
+
+      {/* <span>Name:</span><input type="text" placeholder='Name'/><br />
       <span>Email:</span><input type="text" placeholder='Email'/><br />
       <span>NUmber:  </span><input type="text" placeholder='Number'/><br />
-      <button type='submit'>Submit</button>
+       */}
 
     </div>
   )
