57d741859a7a10263f2f453aea2ae4db8f0621e5
haseebulamin <haseebulamin07@gmail.com>
2024-05-11 23:40:56 +0500

    lable

--- src/Components/React-roadmap-Docs/Practice.jsx
+++ src/Components/React-roadmap-Docs/Practice.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Practice() {
   return (
     <div>
-      <input type="text" placeholder='Name'/><br />
-      <input type="text" placeholder='Email'/><br />
-      <input type="text" placeholder='Number'/><br />
+
+      <span>Name:</span><input type="text" placeholder='Name'/><br />
+      <span>Email:</span><input type="text" placeholder='Email'/><br />
+      <span>NUmber:  </span><input type="text" placeholder='Number'/><br />
       <button type='submit'>Submit</button>
 
     </div>
