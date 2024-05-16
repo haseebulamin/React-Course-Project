5ae8ecc8f8aa9d73e9898439cf0f52b5eda01f39
haseebulamin <haseebulamin07@gmail.com>
2024-05-16 23:07:00 +0500

    age

--- src/Components/React-roadmap-Docs/Prompt.jsx
+++ src/Components/React-roadmap-Docs/Prompt.jsx
@@ -3,6 +3,13 @@ import React from 'react'
 export default function Prompt() {
     const click = ()=>{
         prompt("Enter Your age: ");
+        if(age<18)
+          {
+            alert("You are under 18.")
+          }
+          else{
+            alert("You are 18.")
+          }
     }
   return (
     <div>
