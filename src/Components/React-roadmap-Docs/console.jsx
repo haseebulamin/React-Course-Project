fc7fe5ad0f9c21e8532b015d48f89bb9cf2a1c42
haseebulamin <haseebulamin07@gmail.com>
2024-11-30 20:25:43 +0500

    console

---
+++ src/Components/React-roadmap-Docs/console.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function console() {
+    console.log("first");
+    console.log("two");
+    console.log("three");
+  return (
+    <div>
+        <h1>first</h1>
+        <h1>two</h1>
+        <h1>three</h1>
+    </div>
+  )
+}
