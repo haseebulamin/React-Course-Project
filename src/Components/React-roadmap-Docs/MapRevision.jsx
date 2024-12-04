fa166560be86ae0e403f31b842f64f0d5fdc869d
haseebulamin <haseebulamin07@gmail.com>
2024-12-04 20:46:08 +0500

    map table

--- src/Components/React-roadmap-Docs/MapRevision.jsx
+++ src/Components/React-roadmap-Docs/MapRevision.jsx
@@ -36,6 +36,24 @@ export default function MapRevision() {
           ))}
         </tbody>
       </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Contact</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((data, i) => (
+            <tr key={i}>
+              <td>{data.name}</td>
+              <td>{data.email}</td>
+              <td>{data.contact}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
