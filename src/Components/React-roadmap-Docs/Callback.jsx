b5d2f3f8ed49f55972cca7508844cb20288c1f5a
haseebulamin <haseebulamin07@gmail.com>
2024-12-09 22:37:27 +0500

    alert 1 2 3

--- src/Components/React-roadmap-Docs/Callback.jsx
+++ src/Components/React-roadmap-Docs/Callback.jsx
@@ -7,9 +7,21 @@ export default function Callback() {
             alert("Hello, i'm alert.")
         });
     }
+    const alert1 = ()=>{
+        setTimeout(() => {
+            alert("Hello, i'm alert.")
+        },2000);
+    }
+    const alert2 = ()=>{
+        setTimeout(() => {
+            alert("Hello, i'm alert.")
+        },3000);
+    }
   return (
     <div>
         <button onClick={alert}>Click</button>
+        <button onClick={alert1}>Click</button>
+        <button onClick={alert2}>Click</button>
     </div>
   )
 }
